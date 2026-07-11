061a406a5668f4e4445a559c99ba4ab3d035d991
Caleb Adebayo <calebtolorunleke@yahoo.com>
2026-07-10 19:24:11 -0700

    Set up QuickDine frontend architecture

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import "../src/index.css";
+import Home from "./pages/Home";
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Home />} path="/" />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const Home = () => {
+  return (
+    <div className="min-h-screen bg-#f9f9f9 flex flex-col pt-0">
+      <Navbar />
+    </div>
+  );
+};
+
+export default Home;
